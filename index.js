784b5accd586b6a004bd16a73ec585389b2e44f7
Michael Aghumyan <mikaelaghumyan@outlook.com>
2022-03-23 19:35:17 +0400

    initial db connection

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
 const PORT = 3000 || process.env.port;
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI) {
+  throw new Error("Must provide");
+}
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -8,5 +21,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log(`App is listening on port ${port}`);
+  else console.log(`App is listening on port ${PORT}`);
 });
