39264199c9919b05a5e64953ac206d988e9b8267
Michael Aghumyan <mikaelaghumyan@outlook.com>
2022-04-02 16:21:42 +0400

    students page

--- frontend/src/pages/students.js
+++ frontend/src/pages/students.js
@@ -1,5 +1,34 @@
+import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Students = () => {
-  return <div>Students</div>;
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const res = await axios.get("/api/students", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("coach")).token}`,
+        },
+      });
+      console.log(res.data.students);
+
+      setStudents(res.data.students);
+    };
+
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div>
+      <Navbar currentPage="students" redirectPage="workshops" />
+    </div>
+  );
 };
 
 export default Students;
