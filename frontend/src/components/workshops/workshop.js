08dad7beb4b6cbaa7835766ec69b57386a025396
Michael Aghumyan <mikaelaghumyan@outlook.com>
2022-04-10 23:36:18 +0400

    add workshop delete functionality

--- frontend/src/components/workshops/workshop.js
+++ frontend/src/components/workshops/workshop.js
@@ -8,6 +8,22 @@ const Workshop = (props) => {
       <th>{props.workshop._id.substr(1, 10)}</th>
       <td>{props.workshop.name}</td>
       <td>{props.workshop.level}</td>
+      <td>
+        <button onClick className="button is-info is-outlined is-light">
+          Students
+        </button>{" "}
+        <button onClick className="button is-primary is-outlined is-light">
+          More Info
+        </button>{" "}
+        <button
+          onClick={() => {
+            props.deleteWorkshop(props.workshop._id);
+          }}
+          className="button is-danger is-outlined is-light"
+        >
+          Delete
+        </button>{" "}
+      </td>
     </tr>
   );
 };
